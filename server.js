3b32a32a35e20df600db3fa3f4523a814fe2c677
Thawon Uttamavanit <uttamavanit@gmail.com>
2014-09-13 19:30:29 +1000

    1

---
+++ server.js
@@ -0,0 +1,12 @@
+﻿var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser");
+
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/app"));
+
+app.listen(app.get("port"), function () {
+    console.log("Now serving the app at http://localhost:3000");
+});
