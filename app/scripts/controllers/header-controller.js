8a7f291f54ef9f8d2054147e670cf5dc2fb0b896
Thawon Uttamavanit <uttamavanit@gmail.com>
2014-09-18 16:16:28 +1000

    test side menu

--- app/scripts/controllers/header-controller.js
+++ app/scripts/controllers/header-controller.js
@@ -7,8 +7,10 @@
                 var bannerImageChange = function () {
                     if (util.isBreakpoint("xs")) {
                         $("#hd_banner_img").attr("src", "/images/tree-small.png");
+                        $("#test_div").show();
                     } else {
                         $("#hd_banner_img").attr("src", "/images/tree-big.png");
+                        $("#test_div").hide();
                     }
                 }
 
--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -4,6 +4,12 @@ define(
         app.controller("HomeController", ["$scope",
             function ($scope) {
                 $scope.name = "main page";
+
+                $(document).ready(function () {
+                    $('[data-toggle="offcanvas"]').click(function () {
+                        $('.row-offcanvas').toggleClass('active')
+                    });
+                });
             }
         ]);
     });
